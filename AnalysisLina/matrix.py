1c79d9cfd0e89d66ea14a8c1bf54804c3765c4b2
EricAntosch <ericantosch@web.de>
2020-05-04 21:38:16 +0200

    NextCommit

--- AnalysisLina/matrix.py
+++ AnalysisLina/matrix.py
@@ -4,11 +4,16 @@ import numpy as np
 
 
 def main():
-    A = np.array([[1, 1, -1],
-                    [35, -25, 0],
-                    [0, 25, 80]])
-    B = np.array([0, -9, 24])
+    """ A = np.array([[2, -10],
+                    [10, -8.7]])
 
+    B = np.array([39,-3]) """
+
+    A = np.array([[1, -1, 1],
+                    [8, 10, 0],
+                    [0, 10, 999999]])
+
+    B = np.array([0,39, 23])
     C = np.linalg.solve(A, B)
     print(C)
     print(5.8*(1/26))
